baee8c75a4d1e31c424b4dc8c5f788b1d54e9b52
Pavel Golikov <paulgolikov@gmail.com>
2026-01-07 01:43:36 -0500

    numerical wrappers results

--- experiments/run_job.sh
+++ experiments/run_job.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=golikovp_job
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:h100:2
-#SBATCH --cpus-per-task=2
+#SBATCH --cpus-per-task=4
 #SBATCH --mem=64G
 #SBATCH --time=02:00:00
 #SBATCH --output=slurm_out.out
@@ -19,4 +19,4 @@ export NLTK_DATA=/project/aip-gpekhime/golikovp/nltk_data
 mkdir -p $HF_HOME
 
 # Running 5 samples per problem for averaging
-python evaluate.py --name interleaved_substitutions --n_samples 5 --limit 30 --model "GAIR/LIMO-v2"
+python evaluate.py --name numerical_wrappers --n_samples 5 --limit 30 --model "GAIR/LIMO-v2"
