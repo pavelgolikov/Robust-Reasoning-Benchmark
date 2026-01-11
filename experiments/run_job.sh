2a8fe4369ac88c1a8e2f5fa7e766612d083d4caf
Pavel Golikov <paulgolikov@gmail.com>
2026-01-11 18:41:48 -0500

    not not results

--- experiments/run_job.sh
+++ experiments/run_job.sh
@@ -19,4 +19,4 @@ export NLTK_DATA=/project/aip-gpekhime/golikovp/nltk_data
 mkdir -p $HF_HOME
 
 # Running 5 samples per problem for averaging
-python evaluate.py --name split_indices --num_distractors 64 --n_samples 5 --limit 30 --model "GAIR/LIMO-v2"
+python evaluate.py --name not_not_yot --n_samples 5 --limit 30 --model "GAIR/LIMO-v2"
