c40e2b4c0f9f91e50fe53bb341149f56debb6ccf
Pavel Golikov <paulgolikov@gmail.com>
2026-01-18 15:36:52 -0500

    results for semantic analysis.

--- experiments/run_job.sh
+++ experiments/run_job.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --job-name=golikovp_job
 #SBATCH --nodes=1
-#SBATCH --gres=gpu:h100:2
+#SBATCH --gres=gpu:l40s:1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=64G
-#SBATCH --time=12:00:00
+#SBATCH --time=2:00:00
 #SBATCH --output=slurm_out.out
 #SBATCH --error=slurm_out.err
 #SBATCH --account=aip-gpekhime
@@ -20,4 +20,5 @@ mkdir -p $HF_HOME
 
 # Running 5 samples per problem for averaging
 # python evaluate.py --name interleaved_context --n_samples 5 --limit 30 --model "GAIR/LIMO-v2"
-python evaluate.py --names all --n_samples 5 --limit 30 --model "GAIR/LIMO-v2" > slurm_out.out
+# python evaluate.py --names all --n_samples 5 --limit 30 --model "GAIR/LIMO-v2" > slurm_out.out
+python3 analysis/analyze_failures.py --file interleaved_context_word/results/GAIR_LIMO-v2_interleaved_context_word_s42_20260118_040052.json --model_name all-MiniLM-L6-v2
