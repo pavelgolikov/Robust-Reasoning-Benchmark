194aa98494adfd0a57346ab40f53fff7b45fb8f1
Pavel Golikov <paulgolikov@gmail.com>
2026-01-18 16:13:57 -0500

    two types of jobs split

--- experiments/fail_analysis_job.sh
+++ experiments/fail_analysis_job.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --job-name=golikovp_job
+#SBATCH --job-name=golikovp_fail_analysis
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:l40s:1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=64G
 #SBATCH --time=2:00:00
-#SBATCH --output=slurm_out.out
-#SBATCH --error=slurm_out.err
+#SBATCH --output=failure_analysis.out
+#SBATCH --error=failure_analysis.err
 #SBATCH --account=aip-gpekhime
 
 # Run Evaluation
@@ -18,7 +18,4 @@ export XDG_CACHE_HOME=/project/aip-gpekhime/golikovp/cache
 export NLTK_DATA=/project/aip-gpekhime/golikovp/nltk_data
 mkdir -p $HF_HOME
 
-# Running 5 samples per problem for averaging
-# python evaluate.py --name interleaved_context --n_samples 5 --limit 30 --model "GAIR/LIMO-v2"
-# python evaluate.py --names all --n_samples 5 --limit 30 --model "GAIR/LIMO-v2" > slurm_out.out
-python3 analyze_failures.py --names all > failure_analysis.txt
+python3 analysis/analyze_failures.py --names all > failure_analysis.out
