4378f6ec632a7b7c295f76099378cbf07539cb3c
Pavel Golikov <paulgolikov@gmail.com>
2026-01-30 02:05:54 -0500

    results from 30 distractor run

--- experiments/run_job.sh
+++ experiments/run_job.sh
@@ -4,7 +4,7 @@
 #SBATCH --gres=gpu:h100:2
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=64G
-#SBATCH --time=4:00:00
+#SBATCH --time=12:00:00
 #SBATCH --output=eval_out.out
 #SBATCH --error=eval_err.err
 #SBATCH --account=aip-gpekhime
