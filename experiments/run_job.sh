bc092403c8e73ffd563d0c8e358effa56101d47b
Pavel Golikov <paulgolikov@gmail.com>
2026-02-04 14:20:07 -0500

    latest errors in eval_conv

--- experiments/run_job.sh
+++ experiments/run_job.sh
@@ -4,7 +4,7 @@
 #SBATCH --gres=gpu:h100:4
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=128G
-#SBATCH --time=3:00:00
+#SBATCH --time=6:00:00
 #SBATCH --output=eval_out.out
 #SBATCH --error=eval_err.err
 #SBATCH --account=aip-gpekhime
@@ -22,5 +22,5 @@ mkdir -p $HF_HOME
 # python evaluate.py --names rail_fence --n_samples 5 --limit 30 > eval_out.out
 # python evaluate.py --names reversal --n_samples 5 --limit 30 --model "tiiuae/Falcon-H1R-7B" > eval_out.out
 
-# python evaluate_conversation.py --max_model_length 262000 --n_samples 5 --num_distractors 15 --distractors_per_query 5 --num_gpus 4 > eval_out.out
-python evaluate_agent.py --max_model_length 65536 --limit 30 --n_samples 5 --names interleaved_context_word,not_not --num_gpus 4 > eval_out.out
+python evaluate_conversation.py --max_model_length 262000 --sample_range 0 --n_samples 5 --num_distractors 10 --distractors_per_query 5 --num_gpus 4 > eval_out.out
+# python evaluate_agent.py --max_model_length 65536 --limit 30 --n_samples 5 --names interleaved_context_word,not_not --num_gpus 4 > eval_out.out
