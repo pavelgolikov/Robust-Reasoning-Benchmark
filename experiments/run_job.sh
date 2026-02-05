1286d04b4b408d57317234b2568a98e7950ce558
Pavel Golikov <paulgolikov@gmail.com>
2026-02-05 14:10:55 -0500

    results with stale file handle

--- experiments/run_job.sh
+++ experiments/run_job.sh
@@ -26,6 +26,6 @@ export NCCL_IGNORE_DISABLED_P2P=1
 # python evaluate.py --names rail_fence --n_samples 5 --limit 30 > eval_out.out
 # python evaluate.py --names reversal --n_samples 5 --limit 30 --model "tiiuae/Falcon-H1R-7B" > eval_out.out
 
-python evaluate_conversation.py --max_model_length 262000 --sample_range 0-14 --n_samples 5 --num_distractors 16 --distractors_per_query 4 --num_gpus 4 > eval_out.out
+python evaluate_conversation.py --max_model_length 262000 --sample_range 0 --n_samples 5 --num_distractors 16 --distractors_per_query 4 --num_gpus 4 > eval_out.out
 
 # python evaluate_agent.py --max_model_length 65536 --limit 30 --n_samples 5 --names interleaved_context_word,not_not --num_gpus 4 > eval_out.out
