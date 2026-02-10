0df25042f3a8412675e0ea99a2e811858818975e
Pavel Golikov <paulgolikov@gmail.com>
2026-02-10 13:25:28 -0500

    results showing that we need to adjust max model length depending on mode

--- experiments/run_job.sh
+++ experiments/run_job.sh
@@ -4,7 +4,7 @@
 #SBATCH --gres=gpu:h100:4
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=128G
-#SBATCH --time=3:00:00
+#SBATCH --time=1:00:00
 #SBATCH --output=eval_out.out
 #SBATCH --error=eval_err.err
 #SBATCH --account=aip-gpekhime
@@ -27,5 +27,4 @@ export NCCL_IGNORE_DISABLED_P2P=1
 # python evaluate.py --names reversal --n_samples 5 --limit 30 --model "tiiuae/Falcon-H1R-7B" > eval_out.out
 # python evaluate_agent.py --max_model_length 65536 --limit 30 --n_samples 5 --names interleaved_context_word,not_not --num_gpus 4 > eval_out.out
 
-# python evaluate_conversation.py --max_model_length 65536 --n_samples 5 --context_saturation 0 --distractors_per_query 4 --num_gpus 4 > eval_out.out
 python evaluate_conversation.py --max_model_length 65536 --n_samples 5 --context_saturation 0 --distractors_per_query 4 --num_gpus 4 > eval_out.out
