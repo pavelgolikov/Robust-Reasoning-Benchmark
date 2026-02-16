52cbcf91d601e515ab0f790404d4cb1db84465d1
Pavel Golikov <paulgolikov@gmail.com>
2026-02-16 18:28:15 -0500

    rail fence results

--- experiments/run_job.sh
+++ experiments/run_job.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --job-name=golikovp_job
 #SBATCH --nodes=1
-#SBATCH --gres=gpu:h100:4
+#SBATCH --gres=gpu:h100:2
 #SBATCH --cpus-per-task=4
-#SBATCH --mem=128G
+#SBATCH --mem=64G
 #SBATCH --time=3:00:00
 #SBATCH --output=eval_out.out
 #SBATCH --error=eval_out.out
@@ -17,15 +17,14 @@ export HF_HOME=/project/aip-gpekhime/golikovp/cache
 export XDG_CACHE_HOME=/project/aip-gpekhime/golikovp/cache
 export NLTK_DATA=/project/aip-gpekhime/golikovp/nltk_data
 mkdir -p $HF_HOME
-
 # NCCL Fixes
-# export NCCL_DEBUG=INFO
 export NCCL_IGNORE_DISABLED_P2P=1
+# export NCCL_DEBUG=INFO
 # export VLLM_ALLOW_LONG_MAX_MODEL_LEN=1
 
-# python evaluate.py --names rail_fence --n_samples 3 --limit 2 --num_gpus 2 &> eval_out.out
-# python evaluate_agent.py --max_model_length 65536 --limit 2 --n_samples 3 --names interleaved_context_word --num_gpus 2 &> eval_out.out
-python evaluate_context.py --limit 2 --n_samples 3 --context_size 8192 --context_type math --context_file context_math_1M.json --num_gpus 2 &> eval_out.out
+python evaluate.py --names rail_fence --n_samples 5 --num_gpus 2 &> eval_out.out
+# python evaluate_agent.py --max_model_length 65536 --n_samples 5 --names rail_fence --num_gpus 4 &> eval_out.out
+# python evaluate_context.py --limit 2 --n_samples 3 --context_size 8192 --context_type math --context_file context_math_1M.json --num_gpus 2 &> eval_out.out
 
 # python evaluate.py --names reversal --n_samples 5 --limit 30 --model "tiiuae/Falcon-H1R-7B" > eval_out.out
 # python evaluate_agent.py --max_model_length 65536 --limit 30 --n_samples 5 --names interleaved_context_word,not_not --num_gpus 4 > eval_out.out
--- experiments/run_job2.sh
+++ experiments/run_job2.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH --job-name=golikovp_job2
 #SBATCH --nodes=1
-#SBATCH --gres=gpu:h100:4
+#SBATCH --gres=gpu:h100:2
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=64G
 #SBATCH --time=3:00:00
 #SBATCH --output=eval_out2.out
-#SBATCH --error=eval_err2.err
+#SBATCH --error=eval_out2.out
 #SBATCH --account=aip-gpekhime
 
 # Run Evaluation
@@ -18,6 +18,8 @@ export XDG_CACHE_HOME=/project/aip-gpekhime/golikovp/cache
 export NLTK_DATA=/project/aip-gpekhime/golikovp/nltk_data
 mkdir -p $HF_HOME
 
+export NCCL_IGNORE_DISABLED_P2P=1
+
 # TODO: need to run rail_fence eval real quick
 # Running 5 samples per problem for averaging
 # python evaluate.py --names rail_fence --n_samples 5 --num_gpu 4 --limit 30 > eval_out2.out
@@ -25,4 +27,8 @@ mkdir -p $HF_HOME
 
 # python evaluate_conversation.py --max_model_length 262000 --n_samples 5 --num_distractors 15 --distractors_per_query 5 --num_gpus 4 > eval_out2.out
 
-python evaluate_conversation.py --max_model_length 262000 --n_samples 30 --context_fill_lvl 50 --distractors_per_query 4 --num_gpus 4 --new_run > eval_out2.out
+# python evaluate_conversation.py --max_model_length 262000 --n_samples 30 --context_fill_lvl 50 --distractors_per_query 4 --num_gpus 4 --new_run > eval_out2.out
+
+# python evaluate_context.py --limit 2 --n_samples 3 --context_size 8192 --context_type math --context_file context_math_1M.json --num_gpus 2 &> eval_out2.out
+
+python evaluate_agent.py --n_samples 5 --names rail_fence --num_gpus 2 &> eval_out2.out
