6ea8d638d6ffbd6e11876a203d24bf68f4a153cc
Pavel Golikov <paulgolikov@gmail.com>
2026-02-27 19:38:09 -0500

    sanitized results and added correct results with 32000 max model length

--- experiments/run_job.sh
+++ experiments/run_job.sh
@@ -4,7 +4,7 @@
 #SBATCH --gres=gpu:h100:4
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=128G
-#SBATCH --time=4:00:00
+#SBATCH --time=7:00:00
 #SBATCH --output=eval_out.out
 #SBATCH --error=eval_out.out
 #SBATCH --account=aip-gpekhime
@@ -22,7 +22,7 @@ export NCCL_IGNORE_DISABLED_P2P=1
 # export NCCL_DEBUG=INFO
 # export VLLM_ALLOW_LONG_MAX_MODEL_LEN=1
 
-python evaluate.py --names not_not,opposites,rail_fence --dataset HuggingFaceH4/aime_2024 --n_samples 16 --num_gpus 4 &> eval_out.out
+python evaluate.py --names interleaved_context_line,interleaved_context_word,interleaved_context_symbol,rail_fence --dataset HuggingFaceH4/aime_2024 --n_samples 16 --num_gpus 4 &> eval_out.out
 
 # python evaluate_agent.py --max_model_length 65536 --n_samples 5 --names rail_fence --num_gpus 4 &> eval_out.out
 # python evaluate_context.py --limit 1 --n_samples 5 --context_size 8192 --context_type math --context_file context_math_1M.json --num_gpus 2 &> eval_out.out
