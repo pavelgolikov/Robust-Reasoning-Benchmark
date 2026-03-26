b9f3d540133acace346588fbf30ad906208d64dc
Pavel Golikov <paulgolikov@gmail.com>
2026-03-26 15:33:49 -0400

    nemotron 32b on context saturation

--- experiments/run_job.sh
+++ experiments/run_job.sh
@@ -4,9 +4,9 @@
 #SBATCH --gres=gpu:h100:4
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=128G
-#SBATCH --time=5:00:00
-#SBATCH --output=eval_out2.out
-#SBATCH --error=eval_out2.out
+#SBATCH --time=3:00:00
+#SBATCH --output=eval_out.out
+#SBATCH --error=eval_out.out
 #SBATCH --account=aip-gpekhime
 
 # Run Evaluation
@@ -52,8 +52,13 @@ export NCCL_IGNORE_DISABLED_P2P=1
 # python evaluate.py --names compound --num_distractors 1 --model openai/gpt-oss-120b                       --max_model_length 131072 --n_samples 16 --num_gpus 4 &> eval_out.out
 
 # python evaluate.py --names all --temperature 0.7 --top_p 1.0 --model nvidia/OpenReasoning-Nemotron-7B --max_model_length 32000  --n_samples 16 --num_gpus 4 &> eval_out.out
+<<<<<<< HEAD
 # python evaluate_context.py --model nvidia/OpenReasoning-Nemotron-7B --context_size 98304 --context_type math --context_file /home/golikovp/projects/aip-gpekhime/golikovp/Linguistic_traps/experiments/context_saturation/contexts/context_math_98304_Nemotron-7B.json  --n_samples 16 --num_gpus 4 &> eval_out.out
 python evaluate_context.py --model nvidia/OpenReasoning-Nemotron-32B --context_size 98304 --context_type math --context_file /home/golikovp/projects/aip-gpekhime/golikovp/Linguistic_traps/experiments/context_saturation/contexts/context_math_98304_Nemotron-32B.json  --n_samples 16 --num_gpus 4 &> eval_out2.out
+=======
+# python evaluate_context.py --model nvidia/OpenReasoning-Nemotron-7B --context_size 98304 --context_type math --context_file /home/golikovp/projects/aip-gpekhime/golikovp/Linguistic_traps/experiments/context_saturation/contexts/context_math_98304_Nemotron-7B.json --max_model_len 32000  --n_samples 16 --num_gpus 4 &> eval_out.out
+python evaluate_context.py --model nvidia/OpenReasoning-Nemotron-32B --context_size 98304 --context_type math --context_file /home/golikovp/projects/aip-gpekhime/golikovp/Linguistic_traps/experiments/context_saturation/contexts/context_math_98304_Nemotron-32B.json --n_samples 16 --num_gpus 4 &> eval_out.out
+>>>>>>> 9c0dd6d (nemotron 32b on context saturation)
 
 # LIMO-v2 on baseline (0.6 temp) and 1 or 2 distractors
 
--- experiments/run_job2.sh
+++ experiments/run_job2.sh
@@ -32,4 +32,4 @@ export NCCL_IGNORE_DISABLED_P2P=1
 
 # python evaluate.py --names rectangle_perimeter,snake_horizontal --temperature 0.7 --top_p 1.0 --model nvidia/OpenReasoning-Nemotron-32B --max_model_length 32000  --n_samples 16 --num_gpus 4 &> eval_out2.out
 
-python evaluate_context.py --model nvidia/OpenReasoning-Nemotron-7B --context_size 98304 --context_type math --context_file /home/golikovp/projects/aip-gpekhime/golikovp/Linguistic_traps/experiments/context_saturation/contexts/context_math_98304_Nemotron-7B.json --max_model_len 32000  --n_samples 16 --num_gpus 4 &> eval_out.out
+python evaluate_context.py --model nvidia/OpenReasoning-Nemotron-7B --context_size 98304 --context_type math --context_file /home/golikovp/projects/aip-gpekhime/golikovp/Linguistic_traps/experiments/context_saturation/contexts/context_math_98304_Nemotron-7B.json --n_samples 16 --num_gpus 4 &> eval_out2.out
