85acea2727c2faf2045cd6b893b710a88de9aa96
Pavel Golikov <paulgolikov@gmail.com>
2026-04-01 15:21:20 -0400

    organized results by date, oss-120b resutls

--- experiments/run_job.sh
+++ experiments/run_job.sh
@@ -24,20 +24,21 @@ export NCCL_IGNORE_DISABLED_P2P=1
 
 
 # compound eval
-python evaluate.py --dataset MathArena/aime_2025 --names baseline                     --model Qwen/Qwen3-30B-A3B-Thinking-2507          --max_model_length 131072  --n_samples 16 --num_gpus 4 &> eval_out.out
-python evaluate.py --dataset MathArena/aime_2025 --names compound --num_distractors 1 --model Qwen/Qwen3-30B-A3B-Thinking-2507          --max_model_length 131072  --n_samples 16 --num_gpus 4 &> eval_out.out
+# python evaluate.py --dataset MathArena/aime_2025 --names baseline                     --model Qwen/Qwen3-30B-A3B-Thinking-2507          --max_model_length 131072  --n_samples 16 --num_gpus 4 &> eval_out.out
+# python evaluate.py --dataset MathArena/aime_2025 --names compound --num_distractors 1 --model Qwen/Qwen3-30B-A3B-Thinking-2507          --max_model_length 131072  --n_samples 16 --num_gpus 4 &> eval_out.out
 
-python evaluate.py --dataset MathArena/aime_2025 --names baseline                     --model nvidia/OpenReasoning-Nemotron-7B          --max_model_length 131072  --n_samples 16 --num_gpus 4 &> eval_out.out
-python evaluate.py --dataset MathArena/aime_2025 --names compound --num_distractors 1 --model nvidia/OpenReasoning-Nemotron-7B          --max_model_length 131072  --n_samples 16 --num_gpus 4 &> eval_out.out
+# python evaluate.py --dataset MathArena/aime_2025 --names baseline                     --model nvidia/OpenReasoning-Nemotron-7B          --max_model_length 131072  --n_samples 16 --num_gpus 4 &> eval_out.out
+# python evaluate.py --dataset MathArena/aime_2025 --names compound --num_distractors 1 --model nvidia/OpenReasoning-Nemotron-7B          --max_model_length 131072  --n_samples 16 --num_gpus 4 &> eval_out.out
 
-python evaluate.py --dataset MathArena/aime_2025 --names baseline                     --model nvidia/OpenReasoning-Nemotron-32B         --max_model_length 131072  --n_samples 16 --num_gpus 4 &> eval_out.out
-python evaluate.py --dataset MathArena/aime_2025 --names compound --num_distractors 1 --model nvidia/OpenReasoning-Nemotron-32B         --max_model_length 131072  --n_samples 16 --num_gpus 4 &> eval_out.out
+# python evaluate.py --dataset MathArena/aime_2025 --names baseline                     --model nvidia/OpenReasoning-Nemotron-32B         --max_model_length 131072  --n_samples 16 --num_gpus 4 &> eval_out.out
+# python evaluate.py --dataset MathArena/aime_2025 --names compound --num_distractors 1 --model nvidia/OpenReasoning-Nemotron-32B         --max_model_length 131072  --n_samples 16 --num_gpus 4 &> eval_out.out
 
-python evaluate.py --dataset MathArena/aime_2025 --names baseline                     --model deepseek-ai/DeepSeek-R1-Distill-Llama-70B --max_model_length 131072  --n_samples 16 --num_gpus 4 &> eval_out.out
+# python evaluate.py --dataset MathArena/aime_2025 --names baseline                     --model deepseek-ai/DeepSeek-R1-Distill-Llama-70B --max_model_length 131072  --n_samples 16 --num_gpus 4 &> eval_out.out
 python evaluate.py --dataset MathArena/aime_2025 --names compound --num_distractors 1 --model deepseek-ai/DeepSeek-R1-Distill-Llama-70B --max_model_length 131072  --n_samples 16 --num_gpus 4 &> eval_out.out
 
-python evaluate.py --dataset MathArena/aime_2025 --names baseline                     --model openai/gpt-oss-120b                       --max_model_length 131072 --n_samples 16 --num_gpus 4 &> eval_out.out
-python evaluate.py --dataset MathArena/aime_2025 --names compound --num_distractors 1 --model openai/gpt-oss-120b                       --max_model_length 131072 --n_samples 16 --num_gpus 4 &> eval_out.out
+# python evaluate.py --dataset MathArena/aime_2025 --names baseline                     --model openai/gpt-oss-120b                       --max_model_length 131072 --n_samples 16 --num_gpus 4 &> eval_out.out
+# python evaluate.py --dataset MathArena/aime_2025 --names compound --num_distractors 1 --model openai/gpt-oss-120b                       --max_model_length 131072 --n_samples 16 --num_gpus 4 &> eval_out.out
+
 
 
 # perturb eval
