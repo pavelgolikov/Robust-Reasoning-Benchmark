5198aa7ac6d3d53c2d512cf8e76f236e0256b14c
Pavel Golikov <paulgolikov@gmail.com>
2026-04-20 03:02:53 -0400

    results for compound 1 distractor

--- experiments/run_job.sh
+++ experiments/run_job.sh
@@ -46,6 +46,22 @@ export NCCL_IGNORE_DISABLED_P2P=1
 # python evaluate.py --dataset MathArena/aime_2025 --names baseline                     --model deepseek-ai/DeepSeek-R1-Distill-Llama-70B --max_model_length 131072  --n_samples 16 --num_gpus 4 &> eval_out.out
 # python evaluate.py --dataset MathArena/aime_2025 --names compound --num_distractors 3 --model deepseek-ai/DeepSeek-R1-Distill-Llama-70B --max_model_length 131072  --n_samples 16 --num_gpus 4 &> eval_out.out
 
+# compound eval on AIME 2024
+python evaluate.py --names baseline                     --model deepseek-ai/DeepSeek-R1-Distill-Llama-70B --max_model_length 131072  --n_samples 16 --num_gpus 4 &> eval_out.out
+
+# python evaluate.py --names baseline                     --model Qwen/Qwen3-30B-A3B-Thinking-2507          --max_model_length 131072  --n_samples 16 --num_gpus 4 &> eval_out.out
+python evaluate.py --names compound --num_distractors 1 --model Qwen/Qwen3-30B-A3B-Thinking-2507          --max_model_length 131072  --n_samples 16 --num_gpus 4 &> eval_out.out
+
+# python evaluate.py --names baseline                     --model nvidia/OpenReasoning-Nemotron-7B          --max_model_length 131072  --n_samples 16 --num_gpus 4 &> eval_out.out
+python evaluate.py --names compound --num_distractors 1 --model nvidia/OpenReasoning-Nemotron-7B          --max_model_length 131072  --n_samples 16 --num_gpus 4 &> eval_out.out
+
+# python evaluate.py --names baseline                     --model openai/gpt-oss-120b                       --max_model_length 131072 --n_samples 16 --num_gpus 4 &> eval_out.out
+python evaluate.py --names compound --num_distractors 1 --model openai/gpt-oss-120b                       --max_model_length 131072 --n_samples 16 --num_gpus 4 &> eval_out.out
+
+# python evaluate.py --names baseline                     --model nvidia/OpenReasoning-Nemotron-32B         --max_model_length 131072  --n_samples 16 --num_gpus 4 &> eval_out.out
+python evaluate.py --names compound --num_distractors 1 --model nvidia/OpenReasoning-Nemotron-32B         --max_model_length 131072  --n_samples 16 --num_gpus 4 &> eval_out.out
+
+python evaluate.py --names compound --num_distractors 1 --model deepseek-ai/DeepSeek-R1-Distill-Llama-70B --max_model_length 131072  --n_samples 16 --num_gpus 4 &> eval_out.out
 
 # context eval
 # python evaluate_context.py --model nvidia/OpenReasoning-Nemotron-7B --context_size 98304 --context_type math --context_file /home/golikovp/projects/aip-gpekhime/golikovp/Linguistic_traps/experiments/context_saturation/contexts/context_math_98304_Nemotron-7B.json --max_model_len 32000  --n_samples 16 --num_gpus 4 &> eval_out.out
