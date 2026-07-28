20380a75b9e2d4bab0e04638fcaa7d66e9edd1a5
Pavel Golikov <paulgolikov@gmail.com>
2026-07-28 12:32:37 -0400

    nemo 32 decode only results

--- experiments/run_job_rebuttal_controls.sh
+++ experiments/run_job_rebuttal_controls.sh
@@ -4,7 +4,7 @@
 #SBATCH --gres=gpu:h100:4
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=128G
-#SBATCH --time=4:00:00
+#SBATCH --time=7:00:00
 #SBATCH --output=eval_out.out
 #SBATCH --error=eval_out.out
 #SBATCH --account=aip-gpekhime
@@ -24,8 +24,8 @@ module load gcc opencv/4.13.0
 module load arrow/25.0.0
 
 # decode recovery eval on AIME 2024
-python evaluate_decode_recovery.py --dataset HuggingFaceH4/aime_2024 --names all --model Qwen/Qwen3-30B-A3B-Thinking-2507          --max_model_length 32000  --max_tokens 32000 --n_samples 16 --num_gpus 4 --temperature 0.7 --top_p 1.0 &> decode_recovery_qwen_aime2024.out
-# python evaluate_decode_recovery.py --dataset HuggingFaceH4/aime_2024 --names all --model nvidia/OpenReasoning-Nemotron-32B         --max_model_length 32000  --max_tokens 32000 --n_samples 16 --num_gpus 4 --temperature 0.7 --top_p 1.0 &> decode_recovery_nemotron32_aime2024.out
+# python evaluate_decode_recovery.py --dataset HuggingFaceH4/aime_2024 --names all --model Qwen/Qwen3-30B-A3B-Thinking-2507          --max_model_length 32000  --max_tokens 32000 --n_samples 16 --num_gpus 4 --temperature 0.7 --top_p 1.0 &> decode_recovery_qwen_aime2024.out
+python evaluate_decode_recovery.py --dataset HuggingFaceH4/aime_2024 --names all --model nvidia/OpenReasoning-Nemotron-32B         --max_model_length 32000  --max_tokens 32000 --n_samples 16 --num_gpus 4 --temperature 0.7 --top_p 1.0 &> decode_recovery_nemotron32_aime2024.out
 # python evaluate_decode_recovery.py --dataset HuggingFaceH4/aime_2024 --names all --model nvidia/OpenReasoning-Nemotron-7B          --max_model_length 32000  --max_tokens 32000 --n_samples 16 --num_gpus 4 --temperature 0.7 --top_p 1.0 &> decode_recovery_nemotron7_aime2024.out
 
 # decode recovery eval on AIME 2025
